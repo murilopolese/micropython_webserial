11cc0b743860fc317bfaa480fb36e4a4a60f6747
Murilo Polese <murilopolese@gmail.com>
2024-07-25 20:40:55 +0200

    Prevent editor taking width larger than viewport

--- views/editor.js
+++ views/editor.js
@@ -4,12 +4,14 @@ import { ReplPanel } from './components/repl-panel.js'
 import { TreePanel } from './components/tree-panel.js'
 
 export function EditorView(state, emit) {
+  // TODO: solve this with css
+  const w = state.isTreePanelOpen ? 'width: calc(100% - 300px)' : ''
   return html`
     <div id="working-area">
       ${Toolbar(state, emit)}
       <div class="row">
         ${TreePanel(state, emit)}
-        <div class="column" style="border-left: solid 2px #DCE1E1;">
+        <div class="column" style="${w}">
           ${CodeEditor(state, emit)}
           ${ReplPanel(state, emit)}
         </div>
