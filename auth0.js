f0e169c22295a5e78920e234f9003911d0d8e2f7
Luca Doglione <l.doglione@arduino.cc>
2024-09-25 10:13:29 +0200

    Hiding Auth0 client, make it not accessible from external code

--- auth0.js
+++ auth0.js
@@ -2,32 +2,47 @@ const APP_URL = "https://lab-micropython.arduino.cc"
 const AUTH0_DOMAIN = "login.arduino.cc"
 const AUTH0_CLIENT_ID = "Nsf3AfKOTw6Au9AGi8ALecA3aN3pVBcH"
 
-var auth0Client;
+class Auth {
+  #auth0Client;
 
-const configureClient = async () => {
-  auth0Client = await auth0.createAuth0Client({
-    domain: AUTH0_DOMAIN,
-    clientId: AUTH0_CLIENT_ID
-  });
-};
+  async configureClient(){
+    this.#auth0Client = await auth0.createAuth0Client({
+      domain: AUTH0_DOMAIN,
+      clientId: AUTH0_CLIENT_ID
+    });
+  };
 
-const login = async () => {
-  await auth0Client.loginWithRedirect({
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  });
-};
+  async isAuthenticated(){
+    return await this.#auth0Client.isAuthenticated();
+  };
 
-const logout = async () => {
-  auth0Client.logout({
-    returnTo: APP_URL
-  });
-};
+  async handleRedirectCallback(){
+    await this.#auth0Client.handleRedirectCallback();
+  };
+
+  async login(){
+    await this.#auth0Client.loginWithRedirect({
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    });
+  };
+
+  async logout(){
+    await this.#auth0Client.logout({
+      returnTo: APP_URL
+    });
+  };
+}
 
 window.onload = async () => {
-  await configureClient();
-  const isAuthenticated = await auth0Client.isAuthenticated();
+  
+  let auth = new Auth();
+  window._logout = async() => {auth.logout()}; 
+  
+  await auth.configureClient();
+
+  const isAuthenticated = await auth.isAuthenticated();  
 
   if(!isAuthenticated){
 
@@ -35,15 +50,18 @@ window.onload = async () => {
     if (query.includes("code=") && query.includes("state=")) {
         // Process the login state
         try{
-            await auth0Client.handleRedirectCallback();
-        } catch (err){
-            window.location = APP_URL
+            await auth.handleRedirectCallback();
+        } catch(err){
+            console.error("Error during redirect: ", error);
         }
+
+        window.location = APP_URL   
             
     } else {
         //Require Login
-        login();
+        auth.login();
     }
     
   }
 }
+
